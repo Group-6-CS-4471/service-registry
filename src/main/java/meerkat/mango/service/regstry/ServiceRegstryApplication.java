0c897fbf62164317ab1055e4402ed11559bb64a1
Joshua Ming <60520554+joshming@users.noreply.github.com>
2023-11-10 11:20:00 -0500

    doing it right (#1)
    Co-authored-by: Joshua Nathan Ming <jming4@uwo.ca>

---
+++ src/main/java/meerkat/mango/service/regstry/ServiceRegstryApplication.java
@@ -0,0 +1,13 @@
+package meerkat.mango.service.regstry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegstryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegstryApplication.class, args);
+	}
+
+}
