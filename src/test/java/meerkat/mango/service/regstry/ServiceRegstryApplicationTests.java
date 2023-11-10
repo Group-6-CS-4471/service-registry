c6484684f2d12e988ea8432723a26054152ffe56
Joshua Nathan Ming <jming4@uwo.ca>
2023-11-10 11:16:14 -0500

    doing it right

---
+++ src/test/java/meerkat/mango/service/regstry/ServiceRegstryApplicationTests.java
@@ -0,0 +1,13 @@
+package meerkat.mango.service.regstry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegstryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
