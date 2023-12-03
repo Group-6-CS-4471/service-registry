0d70ac8def0c9b304352778771ef24d95404faaa
joshming <josh.jm2011@gmail.com>
2023-12-03 18:34:58 -0500

    add cors support in service registry

---
+++ src/main/java/meerkat/mango/service/registry/WebConfig.java
@@ -0,0 +1,17 @@
+package meerkat.mango.service.registry;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .maxAge(3600); // max age in seconds
+    }
+}
